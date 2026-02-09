c5c078d7d8d5d904e42c6a21bae3db3eb1d4b592
sc-vigp <vigp@simcorp.com>
2026-02-09 16:50:22 +0000

    build: Configure Compose Compiler plugin for Kotlin 2.0
    - Add org.jetbrains.kotlin.plugin.compose plugin (version 2.0.21)
    - Remove deprecated composeOptions block
    
    Starting with Kotlin 2.0, the Compose Compiler is a separate Gradle
    plugin and no longer requires the kotlinCompilerExtensionVersion
    configuration in composeOptions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
 }
 
 android {
@@ -42,10 +43,6 @@ android {
     buildFeatures {
         compose = true
         viewBinding = true
-    }
-    
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.4"
     }
     
     packaging {
